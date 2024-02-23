0cbe7cdc7595fd71fbcd491e903e89d1357a641c
dvamedveda <dvamedveya@gmail.com>
2024-02-24 00:37:24 +0600

    Условные конструкции

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,6 +12,19 @@ namespace HelloWorld
             int number = Convert.ToInt32(input);
             Console.WriteLine("Number is: " + number.ToString());
             Console.WriteLine("Multiplied number is: " + (number * number).ToString());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This number is odd number!");
+            }
+            else
+            {
+                Console.WriteLine("This number is not odd number!");
+            }
+            if (number >= 1000)
+            {
+                Console.WriteLine("Number " + number.ToString() + " is greater than 1000.");
+            }
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
